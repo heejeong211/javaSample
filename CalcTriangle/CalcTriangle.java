29fa76d72b6e3a3515d697f3417255fcaf68cdb0
heejeong211 <55972368+heejeong211@users.noreply.github.com>
2020-02-12 12:32:30 +0900

    CalcTriangel
    CalcTriangel

---
+++ CalcTriangle/CalcTriangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class CalcTriangle {
+    public static void main(String[] args) throws Exception {
+        double width, height;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input width: ");
+        // 입력을 숫자가 아닌 문자를 한다면?
+        // 100,000 or 1000.1000.00
+        radius = scanner.nextDouble();
+
+        System.out.println("Area of triangle: " + (width * height / 2.0));
+    }
+}
